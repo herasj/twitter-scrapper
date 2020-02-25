8de541cac19ec70ccb232368570774ac5dffccef
herasj <juanp231100@gmail.com>
2020-02-25 00:29:43 -0500

    Get Tweet

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# twitter-scrapper
+# twitter-scrapper (WIP)
+
+### Los datos importantes se almacenan en un .env
+
+ >USER_EMAIL
+
+ >USER_PASS
+
+ >SEARCH_VAL
--- app.js
+++ app.js
@@ -2,9 +2,10 @@ const puppeteer = require('puppeteer');
 require('dotenv').config();
 const user = process.env.USER_EMAIL;
 const pass = process.env.USER_PASS;
+const search = process.env.SEARCH_VAL;
 (async () => {
   const url = 'https://twitter.com/login'; //Target URL
-  const browser = await puppeteer.launch({headless: false});
+  const browser = await puppeteer.launch({headless: true});
   const page = await browser.newPage();
   await page.goto(url, { waitUntil: 'networkidle0' }) //Esperar
 
@@ -14,8 +15,7 @@ const pass = process.env.USER_PASS;
   await page.waitForSelector('[type=password]');
   await page.type('[type=password]',pass); //Escribe la contraseña en el input de tipo pass
 
-  await page.screenshot({ path: './screenshots/form.png', fullPage: 'true' });//Form SS
-
+  
   await page.waitForSelector('[data-testid="LoginForm_Login_Button"]');
   await page.click('[data-testid="LoginForm_Login_Button"]'); //Click Login
   
@@ -24,21 +24,22 @@ const pass = process.env.USER_PASS;
   await page.click('[data-testid="AppTabBar_Explore_Link"]'); //Click SearchButton
 
   await page.waitForSelector('[data-testid=SearchBox_Search_Input]');
-  await page.type('[data-testid=SearchBox_Search_Input]','Uribe'); //Escribe la contraseña en el input de tipo pass
+  await page.type('[data-testid=SearchBox_Search_Input]',search); //Buscar en twitter
 
-  await page.screenshot({ path: './screenshots/home.png', fullPage: 'true' });//Home SS
 
-  await page.keyboard.press('Enter',{ waitUntil: 'networkidle2' }); //Search 
 
-  await page.waitForSelector('#tweet-text'); //Waait
-  await page.screenshot({ path: './screenshots/search.png'});//Search results
+  await page.keyboard.press('Enter',{ waitUntil: 'networkidle2' }); //Buscar y esperar  
 
-  await page.click('#tweet-text'); //Click tweet
-  await page.screenshot({ path: './screenshots/tweet.png'});
+  await page.waitForSelector('article'); // Esperar a que carguen los articulos
+  await page.screenshot({ path: './screenshots/search.png'});//Resultados
+//[aria-labelledby="accessible-list-10"] > .css-1dbjc4n > div > div > div
+  await page.click('article',{button: 'middle',clickCount:2, delay: 100}); //Click tweet
+  await page.screenshot({ path: './screenshots/tweet.png'});//Tweet SS
 
-  const element = await page.$('[class="css-1dbjc4n r-1igl3o0 r-qklmqi r-1adg3ll"]');
-  const text = await page.evaluate(element => element, element); //.textContent
-  console.log(text);
+  const articleHandle = await page.$$('article'); //Obtener todos los elementos que sean de tipo article
+  console.log("Numero articulos: "+articleHandle.length);
+  const html = await page.evaluate(article => article.textContent, articleHandle);
+  console.dir(html);
 
   await browser.close(); //Close Chromium 
 })();
